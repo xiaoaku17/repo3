6ff04fd3c4bb7111f630983cd7f9e490960815d6
17 <2212446436@qq.com>
2018-11-26 21:15:33 +0800

    2222

--- liuyipeng/src/com/itheima/git/git.java
+++ liuyipeng/src/com/itheima/git/git.java
@@ -11,4 +11,9 @@ public class git {
         System.out.println("hello git");
         System.out.println("hello git");
     }
+    public void num(){
+        int a = 0;
+        a += 10;
+        System.out.println(a);
+    }
 }
